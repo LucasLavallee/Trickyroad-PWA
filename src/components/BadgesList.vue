c9b6e7dba72a17a6f37acdd0141ecb556155b12e
LucasLavallee <lucas.lavallee@outlook.com>
2020-04-03 16:26:07 +0200

    first test ble

--- src/components/BadgesList.vue
+++ src/components/BadgesList.vue
@@ -24,7 +24,7 @@ export default {
         }
     },
     created() {
-        console.log(this.badges)
+       // console.log(this.badges)
     }
 }
 </script>
--- src/views/GameMode.vue
+++ src/views/GameMode.vue
@@ -1,5 +1,39 @@
 <template>
     <div id="gameMode">
         <h1>Game mode</h1>
+        <p @click="connectToESP">CONNECT</p>
+        <p @click="send">SEND DATA</p>
     </div>
 </template>
+
+<script>
+import BluetoothManager from '../class/bluetooth/BluetoothManager'
+
+export default {
+    name: 'GameMode',
+    data() {
+        return {
+            bleManager: null
+        }
+    },
+    methods: {
+        connectToESP() {
+            this.bleManager.connect()
+                .then(() => {
+                    console.log(this.bleManager.getDeviceName() + ' is connected')
+                })
+
+        },
+        send() {
+            this.bleManager.send("0,0")
+        }
+    },
+    mounted() {
+        this.bleManager = new BluetoothManager()
+        this.bleManager.receive = (data) => {
+            console.log("RECEIVING")
+            console.log(data)
+        }
+    }
+}
+</script>
